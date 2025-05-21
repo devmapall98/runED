c636fda5d158d0579a3a321111843e6d2390dcf6
devmapall98 <44513672+devmapall98@users.noreply.github.com>
2025-05-22 00:03:07 +0200

    Version bump to 1.0.0

--- libs/consts.py
+++ libs/consts.py
@@ -5,7 +5,7 @@ class consts():
 	This class contains the constants used in the application.
 	"""
 	CHANNEL			= 'Release'
-	SEMVER			= '0.9.9'
+	SEMVER			= '1.0.0'
 	VERSION 		= {
 		'channel': CHANNEL,
 		'full': f'{CHANNEL}/{SEMVER}',
